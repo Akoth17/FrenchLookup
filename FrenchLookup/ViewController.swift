340b425a48b1e0fcf67b879426df92e69fe36adc
Corbin Halliwill <corbin.halliwill@berkeley.edu>
2015-03-12 21:10:12 -0700

    Retrieve DOM for hardcoded word 'manger'

--- FrenchLookup/ViewController.swift
+++ FrenchLookup/ViewController.swift
@@ -9,17 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var appsWebView: UIWebView!
+    var htmlView: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchCNRTL("manger")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func searchCNRTL(searchTerm: String) {
+        if let cnrtlSearchTerm = searchTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding) {
+            let urlPath = "http://cnrtl.fr/definition/\(cnrtlSearchTerm)"
+            let url = NSURL(string: urlPath)
+            let session = NSURLSession.sharedSession()
+            let task = session.dataTaskWithURL(url!, completionHandler: {data, response, error -> Void in
+                println("Task completed")
+                if(error != nil) {
+                    println(error.localizedDescription)
+                }
+                var err: NSError?
+                
+                var htmlResult = NSString(contentsOfURL: url!, encoding: NSUTF8StringEncoding, error: &err)
+                if(err != nil) {
+                    println("HTMLError \(err!.localizedDescription)")
+                }
+                self.htmlView = htmlResult
+                println(htmlResult)
+                
+            })
+            task.resume()
+        }
+    }
 }
 
